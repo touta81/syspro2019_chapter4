6b80f297dc70a5176cb7dfde21e21ca5db4bc2d7
touta81 <t.touta.tt@gmail.com>
2019-09-26 11:43:05 +0900

    関数の追加

--- servo.py
+++ servo.py
@@ -1,17 +1,12 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
-servo = GPIO.PWM(2, 50)
-servo.start(0.0)
-
-bottom = 2.5
-top = 12.0
+#bottom = 2.5
+#top = 12.0
 
 def setservo(servo,rad):
     duty=(rad+90)*4.7/90+2.5
-    servo.changeDutyCycle(duty)
+    servo.ChangeDutyCycle(duty)
 
 
 GPIO.setmode(GPIO.BCM)
