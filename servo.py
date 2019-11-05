4eeb82c93593fd4dcb00c266ece775e13e8f9bc6
touta81 <t.touta.tt@gmail.com>
2019-11-05 09:50:00 +0900

    first commit

--- servo.py
+++ servo.py
@@ -7,19 +7,25 @@ servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
 bottom = 2.5
-middle = 7.2
 top = 12.0
 
+def setservo(servo,rad):
+    duty=(rad+90)*4.7/90+2.5
+    servo.changeDutyCycle(duty)
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+servo=GPIO.PWM(2,50)
+servo.start(0.0)
+
 for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
-
+    setservo(servo,-90)
+    time.sleep(1.0)
+    setservo(servo,0)
+    time.sleep(1.0)
+    setservo(servo,90)
+    time.sleep(1.0)
 
 
 
